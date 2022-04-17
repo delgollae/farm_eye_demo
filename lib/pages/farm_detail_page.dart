f2e132235a0a4f2054bd5a04a2c6f0e09c689729
mdelgolla <mdelgolla@gmail.com>
2022-04-17 21:00:45 +0530

    view details

---
+++ lib/pages/farm_detail_page.dart
@@ -0,0 +1,19 @@
+import 'package:farm_eye_app/pages/widgets/app_page_container.dart';
+import 'package:flutter/material.dart';
+
+class FarmDetailPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _FarmDetailPageState();
+  }
+}
+
+class _FarmDetailPageState extends State<FarmDetailPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return AppPageContainer();
+  }
+
+
+}
